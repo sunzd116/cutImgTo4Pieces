5fb6da024d24bf9e474e224073e5d312fa42d6b7
孙忠达 <szd@sunzhongdadeMacBook-Pro.local>
2019-03-18 00:03:50 +0800

    解决打包问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,13 @@
 const path = require('path');
-const webpack = require('webpack'); //访问内置的插件
 module.exports = {
   entry: {
     cut4Pieces : './src/index.js',
-    excel_example : './src/test.js',
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'cut4Pieces.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    library: 'cut4Pieces',
   },
-  plugins:[
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[name].js.map',
-      append: '\n//# sourceMappingURL=../../dist/[url]',
-    })
-  ]
 };
